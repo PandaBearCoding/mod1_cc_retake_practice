25f01382e5316dd886ea3131d1f95c97d8c93f0d
github username <github email address>
2020-08-18 10:08:30 -0400

    fourth commit

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -12,12 +12,22 @@ class Menu_item
 
     def Menu_item.all
         @@all
-    end 
-
+    end
 
     def owner 
         Restaurant_owner.all.filter do |owner|
             owner.menu_item == self #menu_item is self 
+    end 
+
+    # helper for the most_expensive class method 
+
+    def average_price
+        self.menu_item.reduce(0){ |sum, price| sum + (price.rating/self.menu_item.count) }
+    end 
+
+    #class 
+    def self.most_expensive_item
+        self.all.max { |menu_item_a, menu_item_b| menu_item_a.average_rating <=> menu_item_b.average_rating }
     end 
 
 end 
